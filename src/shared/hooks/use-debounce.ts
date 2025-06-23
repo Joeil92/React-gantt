07207316fdc8708977bf65276670ac904d3390b0
Joeil92 <93368604+Joeil92@users.noreply.github.com>
2025-06-23 19:06:56 +0200

    Add optionnal callback to use-debounce

--- src/shared/hooks/use-debounce.ts
+++ src/shared/hooks/use-debounce.ts
@@ -1,16 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-function useDebouce<T>(value: T, delay: number) {
+function useDebouce<T>(
+  value: T,
+  delay: number,
+  callback?: (debouncedValue: T) => void
+) {
   const [debouncedValue, setDebouncedValue] = useState<T>(value)
+  const callbackRef = useRef(callback)
+
+  useEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
 
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedValue(value)
+      callback?.(value)
     }, delay)
 
     return () => {
       clearTimeout(handler)
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [value, delay])
 
   return debouncedValue
